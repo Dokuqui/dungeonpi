cd930eccf34af9125775489806900cd747d96606
Illia <doku_isv@icloud.com>
2026-04-01 13:43:17 +0200

    feat: ui init

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ fontSize: '3rem', textShadow: '2px 2px 4px #000' }}>
+        Dungeon API
+      </h1>
+      <p style={{ color: '#8a0303', fontSize: '1.2rem', marginTop: '10px' }}>
+        The client awakens...
+      </p>
+    </div>
+  );
+}
